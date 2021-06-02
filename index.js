167cd200b274042364e1562a027b08380746f8e4
kapi1804 <83668560+kapi1804@users.noreply.github.com>
2021-06-02 19:48:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var randomNumber1 = Math.floor(Math.random()*6)+1;
 var randomNumber2= Math.floor(Math.random()*6)+1;
-var imgRandom1="Images/dice"+randomNumber1+".png";
-var imgRandom2="Images/dice"+randomNumber2+".png";
+var imgRandom1="dice"+randomNumber1+".png";
+var imgRandom2="dice"+randomNumber2+".png";
 document.querySelector(".img1").setAttribute("src",imgRandom1);
 document.querySelector(".img2").setAttribute("src",imgRandom2);
 
